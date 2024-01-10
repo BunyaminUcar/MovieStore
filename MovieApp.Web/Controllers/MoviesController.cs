dd9b49c2cbcc8e996a294894a61ecd36b8cb324a
Benjamin <57809668+BunyaminUcar@users.noreply.github.com>
2024-01-10 23:37:44 +0300

    movie store app

---
+++ MovieApp.Web/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApp.Web.Controllers
+{
+    public class MoviesController : Controller
+    {
+        //localhost:42851/movies
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string List()
+        {
+            return "Deneme";
+        }
+        //localhost:42851/movies/details
+        public string Details() {
+
+            return "Film Detayı";
+        }
+    }
+}
