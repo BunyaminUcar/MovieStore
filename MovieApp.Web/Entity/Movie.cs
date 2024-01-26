a76fa6a7348f436cd714c82598e6ccc8d263bd2b
Benjamin <57809668+BunyaminUcar@users.noreply.github.com>
2024-01-26 18:59:54 +0300

    Create Entity/movie.cs

---
+++ MovieApp.Web/Entity/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace MovieApp.Web.Entity
+{
+    public class Movie
+    {
+
+        public int MovieId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [MaxLength(200)]
+        public string Description { get; set; }
+        public string Director { get; set; }
+        public string[] Players { get; set; }
+        public string ImageURL { get; set; }
+        [Required]
+        public int? GenreId { get; set; }
+
+    }
+}
