870da64544f43b18ca38118b7c7d48ba0f57a5e9
Benjamin <57809668+BunyaminUcar@users.noreply.github.com>
2024-01-27 20:12:25 +0300

    Update Genre.cs

--- MovieApp.Web/Entity/Genre.cs
+++ MovieApp.Web/Entity/Genre.cs
@@ -1,11 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieApp.Web.Entity
 {
     public class Genre
     {
         public int GenreId { get; set; }
-        [Required]
         public string Name { get; set; }
+
+        public List<Movie> Movies { get; set; }
     }
 }
