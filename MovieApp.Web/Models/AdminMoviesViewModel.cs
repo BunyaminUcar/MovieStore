32dacd3fefb67675961db98f1cd034c37badc2a6
Benjamin <57809668+BunyaminUcar@users.noreply.github.com>
2024-01-29 13:17:55 +0300

    Create AdminMoviesViewModel.cs

---
+++ MovieApp.Web/Models/AdminMoviesViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using MovieApp.Web.Entity;
+
+namespace MovieApp.Web.Models
+{
+    public class AdminMoviesViewModel
+    {
+        public List<AdminMovieViewModel> Movies
+        {
+            get; set;
+
+        }
+
+        public class AdminMovieViewModel
+        {
+            public int MovieId { get; set; }
+            public string Title { get; set; }
+            public string ImageURL { get; set; }
+            public List<Genre> Genres { get; set; }
+        }
+
+        public class AdminEditMovieViewModel
+        {
+            public int MovieId { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string ImageURL { get; set; }
+            public List<Genre> SelectedGenres { get; set; }
+        }
+
+    }
+}
