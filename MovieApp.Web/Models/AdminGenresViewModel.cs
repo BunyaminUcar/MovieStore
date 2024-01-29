4144271cdba9170a1a604cebaa5d61f5331d7f00
Benjamin <57809668+BunyaminUcar@users.noreply.github.com>
2024-01-29 18:23:58 +0300

    Create AdminGenresViewModel.cs

---
+++ MovieApp.Web/Models/AdminGenresViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using MovieApp.Web.Entity;
+
+namespace MovieApp.Web.Models
+{
+    public class AdminGenresViewModel
+    {
+        public List<AdminGenreViewModel> Genres { get; set; }
+    }
+
+    public class AdminGenreViewModel
+    {
+        public int GenreId { get; set; }
+
+        public string Name { get; set; }
+
+        public int Count { get; set;}
+
+    }
+
+    public class AdminGenreEditViewModel
+    {
+        public int GenreId { get; set; }
+        public string Name { get; set; }
+        public List<AdminMoviesViewModel.AdminMovieViewModel> Movies { get; set; }
+    }
+}
